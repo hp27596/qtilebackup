276b431cdfa68cdee00398cb4c42cdc448511e22
hp27596 <hp27596@gmail.com>
2022-07-11 10:37:11 +0700

    New backup 2022-07-11 10:37:11

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -12,17 +12,11 @@ function run {
 #starting utility applications at boot time
 xautolock -time 10 -locker 'betterlockscreen -l' -detectsleep -killtime 12 -killer "systemctl suspend" &
 
-emacs --daemon &
+\emacs --daemon &
 nextcloud &
-# lxsession &
-# run nm-applet &
-# run pamac-tray &
-# numlockx on &
 fcitx5 &
 blueman-applet &
 caffeine &
-#flameshot &
-#picom --config $HOME/.config/picom/picom.conf &
 picom --config ~/.config/picom/picom.conf --experimental-backends &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 dunst &
@@ -39,17 +33,6 @@ alacritty --class tmux,tmux -e tmux &
 
 python ~/pyscripts/nucleartoast.py &
 
-run emacsclient -c -a "emacs" &
-   #starting user applications at boot time
-# run volumeicon &
-#run discord &
-#nitrogen --random --set-zoom-fill &
-#run caffeine -a &
-#run vivaldi-stable &
-#run firefox &
-#run thunar &
-#run dropbox &
-#run insync start &
-#run spotify &
-#run atom &
-#run telegram-desktop &
+run nuclear &
+
+emacsclient -c -a "emacs" &
