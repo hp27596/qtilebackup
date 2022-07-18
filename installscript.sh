7e77f46c68dc0f6c73a17486552ca17b3551af32
hp27596 <hp27596@gmail.com>
2022-07-18 09:40:25 +0700

    New backup 2022-07-18 09:40:25

--- installscript.sh
+++ installscript.sh
@@ -23,7 +23,7 @@ sudo cp extras/pfetch /usr/bin/pfetch
 rsync -av --progress ./.config/ $HOME/.config --exclude .git
 
 # Install essential packages
-yay -S emacs zsh intel-media-driver intel-gpu-tools va-utils thunar sshfs picom dmenu sway-launcher-desktop xorg-xinput brightnessctl alsa-utils seahorse polkit-gnome gnome-keyring libgnome-keyring bluez bluez-tools bluez-utils speedtest-cli lxappearance-gtk3 material-black-colors-theme dunst perl playerctl pnmixer xautolock cmake fzf feh cmus gnome-disk-utility python-pip python iw net-tools htop mpv tk ctags nodejs npm xclip xsel yarn firewalld picom pacman-contrib neovim gvfs gvfs-mtp
+yay -S emacs zsh intel-media-driver intel-gpu-tools va-utils thunar sshfs picom dmenu xorg-xinput brightnessctl alsa-utils seahorse polkit-gnome gnome-keyring libgnome-keyring bluez bluez-tools bluez-utils speedtest-cli lxappearance-gtk3 material-black-colors-theme dunst perl playerctl pnmixer xautolock cmake fzf feh cmus gnome-disk-utility python-pip python iw net-tools htop mpv tk ctags nodejs npm xclip xsel yarn firewalld picom pacman-contrib neovim gvfs gvfs-mtp
 
 # Install pulseaudio or pipewire. My gpd pocket 2 somehow randomly only works with either one on different distros.
 # pipewire
@@ -40,7 +40,7 @@ systemctl --user enable pulseaudio pulseaudio.socket
 yay -S interception-tools nextcloud-client ranger flameshot ncdu steam ardour fortune-mod aircrack-ng bully reaaver tmux libreoffice-fresh metasploit cowpatty wireshark termshark macchanger pixiewps john android-sdk-platform-tools nerd-fonts-jetbrains-mono evince krita xorg-fonts gucharmap qbittorrent rustscan cpupower-gui wine-staging bottom vlc tldr lutris fcitx5 fcitx5-gtk fcitx5-qt fcitx5-unikey kcm-fcitx5 btop clipmenu gparted pass pass-otp sxiv scrcpy qutebrowser trash-cli kdeconnect
 
 # Install AUR packages
-yay -S auto-cpufreq-git betterlockscreen caffeine-ng cava cmus-notify google-chrome i3lock-color material-black-colors-theme ncmatrix nerd-fonts-ubuntu-mono pyrit python-pulsectl sway-launcher-desktop ticker timeshift-bin noto-fonts-emoji-apple mangohud-git ttf-unifont ttf-font-awesome otf-font-awesome protonvpn goverlay ttf-ubuntu-font-family ttf-ms-fonts nuclear-player-bin routersploit-git
+yay -S auto-cpufreq-git betterlockscreen caffeine-ng cava cmus-notify google-chrome i3lock-color material-black-colors-theme ncmatrix nerd-fonts-ubuntu-mono pyrit python-pulsectl ticker timeshift-bin noto-fonts-emoji-apple mangohud-git ttf-unifont ttf-font-awesome otf-font-awesome protonvpn goverlay ttf-ubuntu-font-family ttf-ms-fonts nuclear-player-bin routersploit-git
 
 # Post installation
 # refresh font cache
